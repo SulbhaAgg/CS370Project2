1e60526103a15a79dd08182822451aa53425a047
Logan Le <loggannlee@gmail.com>
2020-04-30 23:17:20 -0400

    Set up front end development with Vue Js and node.js

--- front_end/server/index.js
+++ front_end/server/index.js
@@ -7,6 +7,14 @@ const app = express();
 //Middleware
 app.use(bodyParser.json());
 app.use(cors());
+//Handle production
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static(__dirname + '/public'));
+    
+    //handle single page application
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 const port = process.env.PORT || 5000;
 
